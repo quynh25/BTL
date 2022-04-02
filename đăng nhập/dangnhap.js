c8983aaa411e0fb0487b0e306dbed5456f05ce70
NT-Huyen <2051060557@e.tlu.edu.vn>
2022-04-02 22:22:37 +0700

    sua dang nhap

---
+++ đăng nhập/dangnhap.js
@@ -0,0 +1,51 @@
+users=[
+    {
+        username: "xuansky94@gmail.com",
+        password: "123456",
+        permission: "admin"
+    },
+    {
+        username: "huyen2001@gmail.com",
+        password: "1234",
+        permission: "admin"
+    },
+    {
+        username: "quynh02@gmail.com",
+        password: "246",
+        permission: "admin"
+    },
+]
+
+
+
+
+function login() {
+    event.preventDefault();
+    var username = myForm.username.value;
+    var password = myForm.password.value;
+
+    var val = check_user(username, password, users)
+    if(val != false){
+        var date = new Date();
+        exp_date = date.getTime() + 10 * 24 * 60 * 60 * 1000;
+        date.setTime(exp_date);
+        document.cookie = "username=" + val.username + ";expires=" + date;
+        document.cookie = "permission=" + val.permission + ";expires=" + date;
+        if(val.permission == "admin"){
+            location.href = "index.html";
+        }
+    }
+    else{
+        alert("your username or password is incorrect")
+    }
+}
+
+function check_user(username, password, users){
+    for(i in users){
+        var user = users[i];
+        if(user.username == username && user.password == password){
+            return true;
+        }
+    }
+    return false;
+}
